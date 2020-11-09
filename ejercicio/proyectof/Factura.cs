a66b447907757c3f88f7bcc27a53a9b603ad9889
Rene Cruz <rfcruz@unah.hn>
2020-11-08 21:34:38 -0600

    cambios

--- ejercicio/proyectof/Factura.cs
+++ ejercicio/proyectof/Factura.cs
@@ -8,7 +8,7 @@ public class Factura
     public string  NumerodeFactura { get; set; }
     public Acreedor Acreedor { get; set; }
     public Proveedor Proveedor { get; set; }
-    public List<Facturadetalle> ListaFacturaDetalle { get; set; }
+    public List<FacturaDetalle> ListaFacturaDetalle { get; set; }
     public double Total { get; set; }
     public double Subtotal { get; set; }
     public double Impuesto { get; set; }
@@ -19,18 +19,17 @@ public class Factura
       NumerodeFactura = numerodeFactura;
       Acreedor = acreedor;
       Proveedor = proveedor;
-      ListaFacturaDetalle = new List<Facturadetalle>();
+      ListaFacturaDetalle = new List<FacturaDetalle>();
       
     }
     public void  AgregarProducto(Producto producto)
     {
         int nuevoCodigo = ListaFacturaDetalle.Count + 1;
         int cantidad = 1;
-        Facturadetalle o = new Facturadetalle(1,1,producto);
+        FacturaDetalle o = new FacturaDetalle(1,1,producto);
         ListaFacturaDetalle.Add(o);
         Total += cantidad * producto.Precio;
         Subtotal = Total * producto.Precio;
         Impuesto = Total * 0.15;
-
     }
 }
--- ejercicio/proyectof/Facturadetalle.cs
+++ ejercicio/proyectof/Facturadetalle.cs
@@ -1,14 +1,15 @@
-public class Facturadetalle
+public class FacturaDetalle
 {
     public int Codigo { get; set; }
     public int Cantidad { get; set; }
     public double Precio { get; set; }
     public Producto Producto { get; set; }
 
-    public Facturadetalle(int codigo, int cantidad, Producto producto)
+    public FacturaDetalle(int codigo, int cantidad, Producto producto)
     {
         Codigo = codigo;
         Cantidad = cantidad;
         Producto = producto;
+        Precio = Producto.Precio;
     }
 }
